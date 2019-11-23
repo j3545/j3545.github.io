e5cbf30e17c3c65e5f6f3e0eb7314de6df2720a0
jedulz <quirozjesus10@gmail.com>
2019-11-23 01:10:41 -0500

    adjusted menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
-function myFunction(x) {
-    x.classList.toggle("change");
+function showNav(x) {
+    
 }
+
+let menu = document.getElementsByClassName("hamburger")[0];
+let menuClone = menu.cloneNode(true);
+
+let isNavOpened = false;
+
+menuClone.addEventListener("click", function(){
+    menuClone.classList.toggle("change");
+    document.getElementById("myNav").style.height = "0%";
+    isNavOpened = false;
+});
+
+document.getElementById("myNav").prepend(menuClone);
+
+
+
+menu.addEventListener("click", function(){
+    if(!isNavOpened){            
+        menuClone.classList.toggle("change");
+        menuClone.style.float = "right";
+        document.getElementById("myNav").style.height = "100%";
+        isNavOpened = true;
+    }
+});
+
+function openNav() {
+    document.getElementById("myNav").style.height = "80%";
+  }
+  
+  function closeNav() {
+    
+  }
